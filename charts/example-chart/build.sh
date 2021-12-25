028479643169f7643ff4b37c92020f0dd77ca62c
samba1112 <76871147+samba1112@users.noreply.github.com>
2021-12-25 18:28:56 +0530

    Update build.sh

--- charts/example-chart/build.sh
+++ charts/example-chart/build.sh
@@ -2,4 +2,4 @@
 apt-get update -y
 apt-get install -y libconfig-yaml-perl
 value=$(perl -MYAML -le 'print YAML::LoadFile(shift)->{image}{tag}'  values.yaml)
-sed -ie "s/tag: $value/tag: sample3/g" values.yaml
+sed -ie "s/tag: $value/tag: sample/g" values.yaml
