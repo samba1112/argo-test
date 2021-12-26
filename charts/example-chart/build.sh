f2737f0850e86f90cbeabd274de3423dc21abe97
samba1112 <76871147+samba1112@users.noreply.github.com>
2021-12-26 21:02:05 +0530

    Delete build.sh

--- charts/example-chart/build.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-apt-get update -y
-apt-get install -y libconfig-yaml-perl
-value=$(perl -MYAML -le 'print YAML::LoadFile(shift)->{image}{tag}'  values.yaml)
-sed -ie "s/tag: $value/tag: sample/g" values.yaml
